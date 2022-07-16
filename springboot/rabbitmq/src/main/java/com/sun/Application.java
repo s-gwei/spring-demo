fcf1abfc138dd89a33aef67544bc644492277dd2
TDsungw <guangwei.sun@china.talkdesk.com>
2022-07-16 16:53:19 +0800

    spring

---
+++ springboot/rabbitmq/src/main/java/com/sun/Application.java
@@ -0,0 +1,17 @@
+package com.sun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: rabbitm启动类
+ * @author: sungw
+ * @create: 2022-07-16 16:46
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
