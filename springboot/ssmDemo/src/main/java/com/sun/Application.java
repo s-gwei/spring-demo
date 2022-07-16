edc0880d0f78e972ec11892d55954215f8f20421
TDsungw <guangwei.sun@china.talkdesk.com>
2022-07-16 17:02:05 +0800

    ssm demo

--- springboot/ssmDemo/src/main/java/com/sun/Application.java
+++ springboot/ssmDemo/src/main/java/com/sun/Application.java
@@ -3,11 +3,15 @@ package com.sun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @description: rabbitm启动类
+ * @author: sungw
+ * @create: 2022-07-16 16:46
+ **/
 @SpringBootApplication
-public class SpringDemoApplication {
-
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(SpringDemoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
